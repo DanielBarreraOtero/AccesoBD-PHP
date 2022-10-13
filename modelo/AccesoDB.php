8fbe0d4bcabbab2a7aff854926d3ab50463217b2
DanielBarreraOtero <dbarote0812@g.educaand.es>
2022-10-13 17:52:25 +0200

    Segundo commit Implementación de acceso a la base de datos finalizada.

---
+++ modelo/AccesoDB.php
@@ -0,0 +1,59 @@
+<?php
+
+function newConn()
+{
+    $con = new PDO("mysql:host=localhost;dbname=accesobd-php", "root", "");
+    return $con;
+}
+
+function insertAlum($con, array $datos): bool
+{
+    $query = "INSERT INTO alumnos(nombre, apellido, foto) VALUES('$datos[0]','$datos[1]','$datos[2]')";
+    return $con->exec($query);
+}
+
+function getAlumById(PDO $con, $id, int $mode = PDO::FETCH_BOTH)
+{
+    $query = "SELECT * FROM alumnos WHERE id = '$id'";
+    $resul = $con->query($query);
+    return $resul->fetch($mode);
+}
+
+function getAllAlums(PDO $con, int $mode = PDO::FETCH_BOTH)
+{
+    $query = "SELECT * FROM alumnos";
+    $resul = $con->query($query);
+    return $resul->fetchAll($mode);
+}
+
+function getAlumPage(
+    PDO $con,
+    int $NPagina,
+    int $NLineas = 10,
+    int $mode = PDO::FETCH_BOTH
+) {
+    $PIni = ($NPagina - 1) * $NLineas + 1;
+    $query = "SELECT * FROM alumnos WHERE id >= $PIni ORDER BY id LIMIT $NLineas";
+    $resul = $con->query($query);
+    return $resul->fetchAll($mode);
+}
+
+function deleteAlumById(PDO $con, $id)
+{
+    $query = "DELETE FROM alumnos WHERE id = $id";
+    return $con->exec($query);
+}
+
+function updateAlumById(PDO $con, $id, $arrayCol, $arrayDatos)
+{
+    $query = "UPDATE alumnos SET ";
+    for ($i = 1; $i <= count($arrayCol); $i++) {
+        if (count($arrayCol) > $i) {
+            $query = $query . $arrayCol[$i - 1] . " = '" . $arrayDatos[$i - 1] . "', ";
+        } else {
+            $query = $query . $arrayCol[$i - 1] . " = '" . $arrayDatos[$i - 1] . "' ";
+        }
+    }
+    $query = $query . "WHERE id = $id";
+    return $con->exec($query);
+}
---
+++ modelo/testbd.php
@@ -0,0 +1,55 @@
+<?php
+
+require_once "AccesoBD-PHP\modelo\AccesoDB.php";
+
+$con = newConn();
+
+
+if (isset($_POST['crear'])) {
+    echo insertAlum($con, ["victor", "esquinas", "foto"]);
+}
+
+if (isset($_POST['select1'])) {
+    var_dump(getAlumById($con, 3, PDO::FETCH_ASSOC));
+}
+
+if (isset($_POST['selectAll'])) {
+    var_dump(getAllAlums($con, PDO::FETCH_ASSOC));
+}
+
+if (isset($_POST['selectPage'])) {
+    var_dump(getAlumPage($con, 1, 4, PDO::FETCH_ASSOC));
+}
+
+if (isset($_POST['borrar'])) {
+    echo deleteAlumById($con, 6);
+}
+
+if (isset($_POST['modificar'])) {
+    echo updateAlumById($con, 5,["Nombre","Apellido"],["Victor","Esquinas"]);
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>testbd</title>
+</head>
+
+<body>
+    <form action="" method="post">
+        <input type="submit" name="crear" value="crear">
+        <input type="submit" name="borrar" value="borrar">
+        <input type="submit" name="modificar" value="modificar">
+        <input type="submit" name="select1" value="select1">
+        <input type="submit" name="selectAll" value="selectAll">
+        <input type="submit" name="selectPage" value="selectPage">
+    </form>
+</body>
+
+</html>
